961b41d876a5e601ff49a65e57aec7b381916e8b
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2022-12-06 16:01:43 +0000

    [Update]6-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  
+  devise_for :admins
+  devise_for :customers
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
@@ -12,22 +13,20 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  ###################################
 
+    # 会員側のルーティング設定
+  root to: 'homes#top'
+
+
+    # 管理者側のルーティング設定
   namespace :admin do
-    resources :items, :genres, :customers, :order_details, :order
-  end
-  namespace :pablic do
-    resources :items, :customers, :cart_items, :orders, :addresses
-  end
-  
-  root 'pablic/homes#top'
-  # get 'pablic/homes#about'
-  
-  # get 'pablic/customers/my_page#show' => '/customers/my_page'
-  # get 'pablic/customers/information#edit' => '/customers/information'
+    resources :items, :customers, :genres, :homes, :orders
+    get 'admin/homes#top' => 'admin/homes#top'
 
-  # get "homes/about" => "homes#about", as: "about"
-  
+  end
+
+
 
   # resources :items, only: [:new, :create, :index, :show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
