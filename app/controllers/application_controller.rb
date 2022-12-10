f40b0405368153a0cb6078f9de0ccf15901a3e4c
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2022-12-10 09:25:34 +0000

    [Update]6-12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
 
   # before_action :configure_permitted_parameters, if: :devise_controller?
+  # def top
+  #   @customer = Customer.find(customer_params)
+  # end
+  # private
+  # def customer_params
+  #   params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted)
+  # end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -19,6 +19,9 @@ class Customer < ApplicationRecord
   def full_name
     self.last_name + " " + self.first_name
   end
+  def full_name_kana
+    self.last_name_kana + " " + self.first_name_kana
+  end
 
 
 end
