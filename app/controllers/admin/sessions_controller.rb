019433f6ed8552bbd52d094a2e6c124c6b6b0e59
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2022-12-10 20:35:08 +0000

    [Update]6-12

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -6,7 +6,7 @@ class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    admin_items_path
+    admin_root_path
   end
 
   def after_sign_out_path_for(resource)
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -7,6 +7,8 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+
+
   end
 
   def update
