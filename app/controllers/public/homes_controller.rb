8f0940270577dc34b012b0afbe1e1ee931c586c2
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2022-12-11 18:55:23 +0000

    [Update]6-12

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,11 +1,17 @@
 class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
+    @items = Item.last(4)
+    # @items = Item.where(:is_active ==true).and(Customer.where(id: [2, 3]))
+
   end
 
   private
   def genre_params
     params.require(:genre).permit(:name)
+  end
+  def item_params
+    params.require(:item).permit(:name, :item_image, :introduction, :price, :is_active)
   end
 
 end
